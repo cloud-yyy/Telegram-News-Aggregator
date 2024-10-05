93ce797b8a0158c1f7fd8b661cceafcd995c7702
Anton Knapp <antonk3344@gmail.com>
2024-10-06 00:19:23 +0300

    Add handling metrics signals endpoints

--- Aggregator/Controllers/LinksMetricsController.cs
+++ Aggregator/Controllers/LinksMetricsController.cs
@@ -9,6 +9,12 @@ namespace Aggregator.Controllers
     [Route("metrics")]
     public class LinksMetricsController : ControllerBase
     {
+        public new class Request
+        {
+            public long UserTelegramId { get; set; }
+            public string Action { get; set; } = string.Empty;
+        }
+
         private readonly IDbContextFactory<ApplicationContext> _contextFactory;
 
         public LinksMetricsController(IDbContextFactory<ApplicationContext> contextFactory)
@@ -17,11 +23,10 @@ namespace Aggregator.Controllers
         }
 
         [HttpGet]
-        [Route("{id:guid}")]
-        public async Task<IActionResult> Handle(Guid id)
+        [Route("link/{id:guid}")]
+        public async Task<IActionResult> HandleLink(Guid id)
         {
             // TODO: remove it
-
             using var context = _contextFactory.CreateDbContext();
 
             var link = await context.WrappedLinks.FindAsync(id);
@@ -42,5 +47,24 @@ namespace Aggregator.Controllers
 
             return RedirectPermanent(link.InnerLink);
         }
+
+        [HttpPost]
+        [Route("signal")]
+        public async Task<IActionResult> HandleMetrics([FromBody] Request request)
+        {
+            var signal = new MetricsSignal()
+            {
+                Id = Guid.NewGuid(),
+                Action = request.Action,
+                UserTelegramId = request.UserTelegramId,
+                ClickedAt = DateTime.UtcNow
+            };
+
+            using var context = _contextFactory.CreateDbContext();
+            context.MetricsSignals.Add(signal);
+            await context.SaveChangesAsync();
+
+            return Ok();
+        } 
     }
 }
